e38ae26782a858e566b3485704a0044a5c9adf32
Mika88 <mikaspivak@gmail.com>
2019-03-28 15:26:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function BoardMember (name, homeState, training) {
   this.training = training
 
   this.veto = function() {
-    return console.log("No, I must disagree");
+    return "No, I must disagree";
   }
 }
